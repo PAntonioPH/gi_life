31b0e270425a9b951300239ed6ac7418fda1961e
pamel <pamela.burgosesp@gmail.com>
2023-09-22 13:59:30 -0600

    Modificaciones de categorías

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -4,8 +4,8 @@ import {useEffect, useState} from "react";
 import {Badge, Box, Flex, Heading, HStack, Image, SimpleGrid, Text} from "@chakra-ui/react";
 import {Product} from "@/interfaces/Product";
 import axios from "axios";
-import product from "@/pages/api/v1/product";
-import {image} from "suneditor/src/plugins";
+import {Pagination} from "@/components/Pagination";
+
 
 const Category = () => {
   const router = useRouter()
@@ -35,18 +35,21 @@ const Category = () => {
     <Layout
       title={typeof category === "string" ? `${category[0].toUpperCase()}${category.slice(1, category.length)}` : "category"}
     >
-      <Flex>
+      <Flex
+        justifyContent={"center"}
+        mt={10}
+      >
         <SimpleGrid
           columns={3}
           spacing={10}
         >
           {
-            data.map(({id, images, discount, name, price, description}) => (
+            data.map(({id, images, discount, name, price}) => (
               <Flex
                 direction={"column"}
                 key={id}
                 boxShadow={"lg"}
-
+                borderRadius={"lg"}
               >
                 <Image
                   src={images[0] && images[0] != "" ? images[0] : "/assets/images/placeholderImg.jpg"}
@@ -54,40 +57,25 @@ const Category = () => {
                   cursor={"pointer"}
                   onClick={() => handleClick(id)}
                   borderRadius={"lg"}
-
-                  // px={5}
-                  // py={5}
-                  // w={"cover"}
-                  // h={"400px"}
+                  h={"300px"}
+                  w={"450px"}
+                  objectFit={"cover"}
                 />
 
                 <Heading
-                  size={"md"}
                   px={5}
-                  my={5}
+                  my={2}
+                  size={"md"}
+                  textAlign={"justify"}
                 >
                   {
-                    name
+                    name.slice(0, 85)
+                  }
+                  {
+                    name.length > 85
+                    && "..."
                   }
                 </Heading>
-
-                <HStack>
-                  <Text
-                    as={discount > 0 ? 'del' : "text"}
-                    color={discount > 0 ? 'red' : "black"}
-                  >
-                    $ {price.toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
-                  </Text>
-
-                  {
-                    discount > 0
-                    && (<Text>
-                      $ {(price - (price * discount) / 100).toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
-                    </Text>)
-                  }
-                </HStack>
-
-
                 {
                   discount > 0
                   && (<Flex
@@ -104,23 +92,64 @@ const Category = () => {
                       Descuento:
                     </Badge>
                     <Text
+                      as={"b"}
                       mx={2}
+                      fontSize={"lg"}
+                      color={"#ff7300"}
                     >
                       {discount}%
                     </Text>
                   </Flex>)
                 }
+                <HStack>
+                  <Text
+                    px={5}
+                    my={2}
+                    fontSize={"lg"}
+                    as={discount > 0 ? 'del' : "text"}
+                    color={discount > 0 ? 'red' : "black"}
+                  >
+                    $ {price.toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+                  </Text>
 
-                <Text
-                  px={5}
-                >
-                  {description}
-                </Text>
+                  {
+                    discount > 0
+                    && (<Text
+                      color={"#00a650"}
+                      fontSize={"lg"}
+                    >
+                      $ {(price - (price * discount) / 100).toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+                    </Text>)
+                  }
+                </HStack>
               </Flex>
             ))
           }
         </SimpleGrid>
       </Flex>
+      {
+        totalPages > 0
+          ? (<Pagination
+            total={totalPages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />)
+          : (<Flex
+            direction={"column"}
+            alignItems={"center"}
+          >
+            <Heading>
+              Aún no hay publicaciones en esta categoría
+            </Heading>
+            <Image
+              mt={5}
+              src={"/assets/images/placeholderImg.jpg"}
+              alt={"404"}
+              w={"50%"}
+            />
+
+          </Flex>)
+      }
     </Layout>
   )
 }

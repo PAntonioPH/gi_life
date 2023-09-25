bffb40a3d69b58cb2b113909c6a8d2f8fff5a8d6
pamel <pamela.burgosesp@gmail.com>
2023-09-25 15:36:33 -0600

    Corrección de categoría en precios con descuento

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -5,13 +5,14 @@ import {Badge, Box, Flex, Heading, HStack, Image, SimpleGrid, Text} from "@chakr
 import {Product} from "@/interfaces/Product";
 import axios from "axios";
 import {Pagination} from "@/components/Pagination";
+import {LoadingPage} from "@/components/LoadingPage";
 
 
 const Category = () => {
   const router = useRouter()
   const [data, setData] = useState<Product[]>([])
 
-  const [loading, setLoading] = useState(true)
+  const [isloading, setLoading] = useState(true)
 
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(0)
@@ -25,7 +26,6 @@ const Category = () => {
       .then((res) => {
         if (res.data.response.product.length > 0) setData(res.data.response.product.sort((a: Product, b: Product) => b.id - a.id))
         setTotalPages(res.data.response.total)
-        console.log(res.data.response.product)
       })
       .finally(() => setLoading(false))
   }, [category, currentPage]);
@@ -35,120 +35,129 @@ const Category = () => {
     <Layout
       title={typeof category === "string" ? `${category[0].toUpperCase()}${category.slice(1, category.length)}` : "category"}
     >
-      <Flex
-        justifyContent={"center"}
-        mt={10}
-      >
-        <SimpleGrid
-          columns={3}
-          spacing={10}
-        >
-          {
-            data.map(({id, images, discount, name, price}) => (
-              <Flex
-                direction={"column"}
-                key={id}
-                boxShadow={"lg"}
-                borderRadius={"lg"}
-              >
-                <Image
-                  src={images[0] && images[0] != "" ? images[0] : "/assets/images/placeholderImg.jpg"}
-                  alt={"Producto"}
-                  cursor={"pointer"}
-                  onClick={() => handleClick(id)}
-                  borderRadius={"lg"}
-                  h={"300px"}
-                  w={"450px"}
-                  objectFit={"cover"}
-                />
-
-                <Heading
-                  px={5}
-                  my={2}
-                  size={"md"}
-                  textAlign={"justify"}
-                >
-                  {
-                    name.slice(0, 85)
-                  }
-                  {
-                    name.length > 85
-                    && "..."
-                  }
-                </Heading>
-                {
-                  discount > 0
-                  && (<Flex
-                    direction={"row"}
-                    alignItems={"center"}
-                    px={5}
-                    my={2}
-                  >
-                    <Badge
-                      borderRadius='lg'
-                      px='2'
-                      py={2}
-                      colorScheme='teal'>
-                      Descuento:
-                    </Badge>
-                    <Text
-                      as={"b"}
-                      mx={2}
-                      fontSize={"lg"}
-                      color={"#ff7300"}
-                    >
-                      {discount}%
-                    </Text>
-                  </Flex>)
-                }
-                <HStack>
-                  <Text
-                    px={5}
-                    my={2}
-                    fontSize={"lg"}
-                    as={discount > 0 ? 'del' : "text"}
-                    color={discount > 0 ? 'red' : "black"}
-                  >
-                    $ {price.toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
-                  </Text>
-
-                  {
-                    discount > 0
-                    && (<Text
-                      color={"#00a650"}
-                      fontSize={"lg"}
-                    >
-                      $ {(price - (price * discount) / 100).toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
-                    </Text>)
-                  }
-                </HStack>
-              </Flex>
-            ))
-          }
-        </SimpleGrid>
-      </Flex>
       {
-        totalPages > 0
-          ? (<Pagination
-            total={totalPages}
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />)
-          : (<Flex
-            direction={"column"}
-            alignItems={"center"}
-          >
-            <Heading>
-              Aún no hay publicaciones en esta categoría
-            </Heading>
-            <Image
-              mt={5}
-              src={"/assets/images/placeholderImg.jpg"}
-              alt={"404"}
-              w={"50%"}
-            />
+        isloading
+          ? (<LoadingPage/>)
+          : (<>
+            <Flex
+              justifyContent={"center"}
+              mt={10}
+            >
+              <SimpleGrid
+                columns={3}
+                spacing={10}
+              >
+                {
+                  data.map(({id, images, discount, name, price}) => (
+                    <Flex
+                      direction={"column"}
+                      key={id}
+                      boxShadow={"lg"}
+                      borderRadius={"lg"}
+                    >
+                      <Image
+                        src={images[0] && images[0] != "" ? images[0] : "/assets/images/placeholderImg.jpg"}
+                        alt={"Producto"}
+                        cursor={"pointer"}
+                        onClick={() => handleClick(id)}
+                        borderRadius={"lg"}
+                        h={"300px"}
+                        w={"450px"}
+                        objectFit={"cover"}
+                      />
 
-          </Flex>)
+                      <Heading
+                        px={5}
+                        my={2}
+                        size={"md"}
+                        textAlign={"justify"}
+                      >
+                        {
+                          name.slice(0, 85)
+                        }
+                        {
+                          name.length > 85
+                          && "..."
+                        }
+                      </Heading>
+
+                      {
+                        discount > 0
+                        && (<Flex
+                          direction={"row"}
+                          alignItems={"center"}
+                          px={5}
+                          my={2}
+                        >
+                          <Badge
+                            borderRadius='lg'
+                            px='2'
+                            py={2}
+                            colorScheme='teal'>
+                            Descuento:
+                          </Badge>
+                          <Text
+                            as={"b"}
+                            mx={2}
+                            fontSize={"lg"}
+                            color={"#ff7300"}
+                          >
+                            {discount}%
+                          </Text>
+                        </Flex>)
+                      }
+
+                      <HStack>
+
+                        <Text
+                          px={5}
+                          my={2}
+                          fontSize={"lg"}
+                          as={discount > 0 ? 'del' : "span"}
+                          color={discount > 0 ? 'red' : "black"}
+                        >
+                          $ {price.toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+                        </Text>
+
+                        {
+                          discount > 0
+                          && (<Text
+                            color={"#00a650"}
+                            fontSize={"lg"}
+                          >
+                            $ {(price - (price * discount) / 100).toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+                          </Text>)
+                        }
+                      </HStack>
+                    </Flex>
+                  ))
+                }
+              </SimpleGrid>
+            </Flex>
+            {
+              totalPages > 0
+                ? (<Pagination
+                  total={totalPages}
+                  currentPage={currentPage}
+                  setCurrentPage={setCurrentPage}
+                />)
+                : (<Flex
+                  direction={"column"}
+                  alignItems={"center"}
+                >
+                  <Heading>
+                    Aún no hay publicaciones en esta categoría
+                  </Heading>
+                  <Image
+                    mt={5}
+                    src={"/assets/images/placeholderImg.jpg"}
+                    alt={"404"}
+                    w={"50%"}
+                  />
+
+                </Flex>)
+            }
+          </>)
       }
     </Layout>
   )

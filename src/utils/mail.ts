0f597d8118b799c133a5d66b06beaea46ac64f19
PAntonioPH <antonio.paph1@gmail.com>
2023-11-07 17:43:40 -0600

    Actualización de correo

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,11 +1,13 @@
 import {Resend} from 'resend';
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const {NEXT_PUBLIC_RESEND_API_KEY, NEXT_PUBLIC_RESEND_EMAIL, NEXT_PUBLIC_RESEND_USER}= process.env;
+
+const resend = new Resend(NEXT_PUBLIC_RESEND_API_KEY);
 
 export const sendEmail = async (to: string, subject: string, message: string) => {
   try {
     const data = await resend.emails.send({
-      from: 'Test <onboarding@resend.dev>',
+      from: `${NEXT_PUBLIC_RESEND_USER} <${NEXT_PUBLIC_RESEND_EMAIL}>`,
       to: [to],
       subject: subject,
       html: message,

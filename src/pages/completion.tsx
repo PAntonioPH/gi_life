d3ca5407d1bc01e5742e04ad8c8c380a73c8771b
pamel <pamela.burgosesp@gmail.com>
2023-11-14 15:01:02 -0600

    Creación de completion.tsx (vista de tu compra realizada después del pago)

--- src/pages/completion.tsx
+++ src/pages/completion.tsx
@@ -1,12 +1,19 @@
-import {Box, Heading} from "@chakra-ui/react";
+import {Box, Flex, Heading, HStack, Image, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack} from "@chakra-ui/react";
 import {Layout} from "@/components/Layout/Layout";
 import {useCart} from "@/hooks/useCart";
-import {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {useRouter} from "next/router";
 import axios from "axios";
 import process from "process";
+import {LoadingPage} from "@/components/LoadingPage";
+import {Purchase} from "@/interfaces/Purchase";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCartArrowDown} from "@fortawesome/free-solid-svg-icons";
+
 
 const Completion = () => {
+
+
   const {clearCart} = useCart()
   const router = useRouter()
 
@@ -14,7 +21,24 @@ const Completion = () => {
 
   const {payment_intent, redirect_status} = router.query
 
+  const [data, setData] = useState<Purchase>({
+    id: 0,
+    date_time_update: "",
+    username: "",
+    time_update: "",
+    date_update: "",
+    id_user: 0,
+    status: "",
+    total: 0,
+    purchase: [],
+    id_purchase_stripe: "",
+  })
+
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
+    if (!payment_intent) return;
+
     axios.get(`/api/v1/purchase/${payment_intent}`,
       {
         params: {
@@ -24,11 +48,13 @@ const Completion = () => {
         headers: {Authorization: `${process.env.NEXT_PUBLIC_TOKEN_WEB}`}
       })
       .then(res => {
+        setData(res.data.response)
         console.log(res.data.response)
       })
       .catch(err => {
         console.log(err)
       })
+      .finally(() => setLoading(false))
 
     if (payment_intent && redirect_status && redirect_status === "succeeded" && payment_intent !== "") {
       clearCartRef.current();
@@ -37,11 +63,153 @@ const Completion = () => {
   }, [payment_intent, redirect_status])
 
   return (<Layout title="Pago completado">
-    <Box>
-      <Heading>
-        Pago completado
-      </Heading>
-    </Box>
+    {
+      loading
+        ? (<LoadingPage/>)
+        : (<Flex
+          direction={"column"}
+          bgGradient="linear(to-b, #00a650, white, white)"
+          borderRadius={"lg"}
+          py={10}
+        >
+
+          <VStack
+            py={10}
+            px={5}
+          >
+            <FontAwesomeIcon fontSize="4rem" icon={faCartArrowDown} color={"white"}/>
+            <Heading
+              color={"#f4f9ff"}
+            >
+              Compra Finalizada
+            </Heading>
+          </VStack>
+
+          <TableContainer
+            px={5}
+          >
+
+            <Table
+              variant={'striped'}
+              mt={5}
+            >
+
+              <Thead bg={"white"}>
+                <Tr>
+                  <Th>
+                    Nombre del Producto
+                  </Th>
+                  <Th>
+                    Producto
+                  </Th>
+                  <Th>
+                    Cantidad
+                  </Th>
+                  <Th>
+                    Precio
+                  </Th>
+                  <Th>
+                    Total
+                  </Th>
+                </Tr>
+              </Thead>
+
+              <Tbody>
+                {
+                  data.purchase.map(({id, name, images, count, discount, price}) => (
+                    <Tr
+                      key={id}
+                    >
+                      <Td>
+                        {
+                          name.slice(0, 30)
+                        }
+                        {
+                          name.length > 30
+                          && "..."
+                        }
+                      </Td>
+                      <Td>
+                        <Image
+                          src={images[0] || '/assets/images/placeholderImg.jpg'}
+                          alt={"Producto"}
+                          borderRadius={"lg"}
+                          w={"70px"}
+                          h={"70px"}
+                        />
+                      </Td>
+                      <Td>
+                        {count}
+                      </Td>
+                      <Td>
+                        <HStack>
+                          <Text
+                            fontSize={"md"}
+                            as={discount > 0 ? 'del' : "span"}
+                          >
+                            $ {price.toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+                          </Text>
+
+                          {
+                            discount > 0
+                            && (<Text
+                              fontSize={"md"}
+                            >
+                              $ {(price - (price * discount) / 100).toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+                            </Text>)
+                          }
+                        </HStack>
+                      </Td>
+                      <Td>
+                        $ {(price * count * (1 - discount / 100)).toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+                      </Td>
+                    </Tr>
+                  ))
+                }
+              </Tbody>
+            </Table>
+          </TableContainer>
+
+          <Flex
+            justifyContent={"flex-end"}>
+            <Text
+              fontSize="2xl"
+              fontWeight="bold"
+              mt={5}
+              mr={10}
+            >
+              Total pagado: $ {(data.total).toLocaleString("es-MX", {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+            </Text>
+          </Flex>
+
+          <Box
+            px={5}
+            py={5}
+          >
+            <Box
+              boxShadow={"lg"}
+              borderRadius={"lg"}
+              py={10}
+              px={5}
+              mt={5}
+              borderLeft={"6px solid #00347f"}
+              bg={"#f5f5f5"}
+              textAlign={"center"}
+            >
+              <Heading
+                size={"md"}
+              >
+                La entrega estimada de tu paquete es:
+              </Heading>
+              <Heading
+                size={"md"}
+              >
+                fecha
+              </Heading>
+            </Box>
+          </Box>
+        </Flex>)
+    }
   </Layout>)
 }
 
